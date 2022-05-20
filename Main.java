0ff362defcaf3ec62afce9bd40ece1664c9a55bd
Apurba Kumar Majumder <9851apurba@gmail.com>
2022-05-20 21:04:16 +0530

    propertise & behavours in oops

--- Main.java
+++ Main.java
@@ -1,6 +1,22 @@
 class Main{
     public static void main(String[] args) {
+        System.out.println("My name is Apurba");
         // create an object
         
+    }
+}
+
+class Human{
+    // properties
+    int age;
+    int weight;
+
+    // behaviour
+    void sleep(){
+        System.out.println("Bhaiya is sleeping");
+    }
+
+    void eat() {
+        System.out.println("Bhaiya is eating");
     }
 }
