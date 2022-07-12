581865cb2ef50a013b1bd14831cd511c752be7bc
Apurba Kumar Majumder <9851apurba@gmail.com>
2022-07-13 00:47:51 +0530

    bankaccount class implemented

--- sumit sir/Main.java
+++ sumit sir/Main.java
@@ -21,16 +21,27 @@ class Main {
             System.out.println(this.name + "[" + this.age + "] says hi.");
         }
     }
-    public static void main(String[] args) {
-        Person p1 = new Person();
-        p1.age = 10;
-        p1.name = "A";
-        p1.sayHi();
 
-        Person p2 = new Person();
-        p2.age = 20;
-        p2.name = "B";
-        p2.sayHi();
+    static class BankAccount {
+        int anum;
+        int money;
+        static int roi;
+
+        void announce(){
+            System.out.println(this.anum + ", " + this.money + ", " + this.roi);
+        }
+    }
+
+    public static void main(String[] args) {
+        // Person p1 = new Person();
+        // p1.age = 10;
+        // p1.name = "A";
+        // p1.sayHi();
+
+        // Person p2 = new Person();
+        // p2.age = 20;
+        // p2.name = "B";
+        // p2.sayHi();
 
         // Person p3 = p2;
         // p3.sayHi();
@@ -55,10 +66,25 @@ class Main {
         // p2 = temp;
 
         // with swap function
-        swap(p1, p2);
+        // swap(p1, p2);
 
-        p1.sayHi();
-        p2.sayHi();
+        // p1.sayHi();
+        // p2.sayHi();
+
+        BankAccount a1 = new BankAccount();
+        a1.anum = 10;
+        a1.money = 100;
+        a1.roi = 4;
+
+        a1.announce();
+
+        BankAccount a2 = new BankAccount();
+        a2.anum = 11;
+        a2.money = 110;
+        a2.roi = 5;
+
+        a2.announce();
+        a1.announce();
     }
 
     public static void swap(Person a, Person b){
