c4edf8600fa6efe4121bc5b59cdf76c5689662ad
Apurba Kumar Majumder <9851apurba@gmail.com>
2022-07-12 22:14:09 +0530

    try swapping with and without a function

--- sumit sir/Main.java
+++ sumit sir/Main.java
@@ -32,21 +32,39 @@ class Main {
         p2.name = "B";
         p2.sayHi();
 
-        Person p3 = p2;
-        p3.sayHi();
+        // Person p3 = p2;
+        // p3.sayHi();
 
-        p2.name = "C";
+        // p2.name = "C";
+        // p2.sayHi();
+        // p3.sayHi();
+
+        // // heep can also store references
+        // Rectangle r = new Rectangle(); // instance, ref
+        // r.tl = new Point(); // instance, reference
+        // r.tl.x = 2;
+        // r.tl.y = 6;
+
+        // r.br = new Point(); // inst, ref
+        // r.br.x = 6;
+        // r.br.y = 1;
+
+        // without a swap function
+        // Person temp = p1;
+        // p1 = p2;
+        // p2 = temp;
+
+        // with swap function
+        swap(p1, p2);
+
+        p1.sayHi();
         p2.sayHi();
-        p3.sayHi();
+    }
 
-        // heep can also store references
-        Rectangle r = new Rectangle(); // instance, ref
-        r.tl = new Point(); // instance, reference
-        r.tl.x = 2;
-        r.tl.y = 6;
-
-        r.br = new Point(); // inst, ref
-        r.br.x = 6;
-        r.br.y = 1;
+    public static void swap(Person a, Person b){
+        Person temp = a;
+        a = b;
+        b = temp;
+        // the swap function because the memory was interchanged in the function only and was not passed on to the actual object.
     }
 }
