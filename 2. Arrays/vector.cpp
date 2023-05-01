ac0f7c853d66c65a4ff9f7572654a8532ff27658
ApurbaKumarMajumder <9851apurba@gmail.com>
2023-05-01 23:52:06 +0530

    improvement

--- 2. Arrays/vector.cpp
+++ 2. Arrays/vector.cpp
@@ -136,22 +136,38 @@ int main() {
     // 4. Pair Sum
 
     vector<int> arr{10, 20, 30, 40, 50, 60, 70};
+    int sum = 80;
 
     // print all pairs
     // outer loop will traverse for each element
-    for (size_t i = 0; i < arr.size(); i++)
-    {
-        cout << "We are at element: " << arr[i] << "\n";
+    // for (size_t i = 0; i < arr.size(); i++)
+    // {
+    //     cout << "We are at element: " << arr[i] << "\n";
+    //     int element = arr[i];
+
+    //     // for every element, will traverse on aage wale elements
+    //     for (size_t j = i+1; j < arr.size(); j++)
+    //     {
+    //         cout << "pair " << element << " with " << arr[j] << "\n";
+    //         // cout << "(" << element << ", " << arr[j] << ")" << "\n"; 
+    //     }
+    // }
+    
+    for (size_t i = 0; i < arr.size(); i++) {
         int element = arr[i];
 
         // for every element, will traverse on aage wale elements
         for (size_t j = i+1; j < arr.size(); j++)
         {
-            cout << "pair " << element << " with " << arr[j] << "\n";
-            // cout << "(" << element << ", " << arr[j] << ")" << "\n"; 
+            if (element + arr[j] == sum)
+            {
+                cout << "Pair Found " << element << "," << arr[j] << "\n";
+            }
+            
         }
+        
     }
-    
+
     
 
     return 0;
