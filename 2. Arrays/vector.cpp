20ee8c294e6dc41012371288e3164fd6c53dd4bb
ApurbaKumarMajumder <9851apurba@gmail.com>
2023-05-03 12:07:52 +0530

    sort 0's and 1's

--- 2. Arrays/vector.cpp
+++ 2. Arrays/vector.cpp
@@ -194,8 +194,30 @@ int main() {
 
     // 6. Sort 0's & 1's
 
+    vector<int> arr{0, 1, 0, 1, 1, 0, 1, 0, 1, 1};
     
-    
+    int start = 0;
+    int end = arr.size() - 1;
+    int i = 0;
 
+    while (i != end)
+    {
+        if (arr[i] == 0) {
+            swap(arr[start], arr[i]);
+            start++;
+            i++;
+        }
+        else {
+            swap(arr[end], arr[i]);
+            end--;
+        }
+    }
+
+    // print
+    for (auto value: arr) {
+        cout << value << " ";
+    }
+    cout << "\n";
+    
     return 0;
 }
