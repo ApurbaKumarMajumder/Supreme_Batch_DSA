e4f8d74815ee267f6cfaccc59e264830a6ad11e6
ApurbaKumarMajumder <9851apurba@gmail.com>
2023-05-05 00:08:41 +0530

    initialisation of 2D array

---
+++ 2. Arrays/array_2D.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // declare 2D array
+    int arr[3][3];
+
+    // initialisation
+    int arr2[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9} };
+
+    cout << arr2[2][0] << "\n";
+}
