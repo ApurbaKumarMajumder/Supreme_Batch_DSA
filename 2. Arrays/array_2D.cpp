aa35b49b8add5d0a4d3bf429b5da3e797b7768b2
ApurbaKumarMajumder <9851apurba@gmail.com>
2023-05-05 12:13:43 +0530

    row wise and column wise printing

--- 2. Arrays/array_2D.cpp
+++ 2. Arrays/array_2D.cpp
@@ -12,4 +12,31 @@ int main() {
         {7, 8, 9} };
 
     cout << arr2[2][0] << "\n";
+
+    // row-wise print
+    // outer loop
+    for (int i=0; i<3; i++) {
+        // for every row, we need to print value in each column
+        for (size_t j = 0; j < 3; j++)
+        {
+            cout << arr2[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+    cout << "printing column wise " << "\n";
+
+    // column-wise print
+    // outer loop
+    for (int i=0; i<3; i++) {
+        // for every row, we need to print value in each column
+        for (size_t j = 0; j < 3; j++)
+        {
+            cout << arr2[j][i] << " ";
+        }
+        cout << "\n";
+    }
+
+
+    return 0;
 }
