a1970e310966e783ef58a551382839678198dca5
ApurbaKumarMajumder <9851apurba@gmail.com>
2023-05-07 22:36:35 +0530

    more of vector 2D

--- 2. Arrays/vector_2D.cpp
+++ 2. Arrays/vector_2D.cpp
@@ -1,23 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+
 int main() {
     // vector of vectors
-    vector<vector<int> > arr;
+    // vector<vector<int> > arr;
 
-    vector<int> a{1, 2, 3};
-    vector<int> b{4, 5, 6};
-    vector<int> c{7, 8, 9};
+    // vector<int> a{1, 2, 3};
+    // vector<int> b{4, 5, 6};
+    // vector<int> c{7, 8, 9};
 
-    arr.push_back(a);
-    arr.push_back(b);
-    arr.push_back(c);
+    // arr.push_back(a);
+    // arr.push_back(b);
+    // arr.push_back(c);
 
-    for (size_t i = 0; i < arr.size(); i++)
-    {
+    // for (size_t i = 0; i < arr.size(); i++)
+    // {
+    //     for (int j=0; j<arr[i].size(); j++) {
+    //         cout << arr[i][j] << " ";
+    //     }
+    //     cout << "\n";
+    // }
+
+    // initialize this way too
+    int rows = 3;
+    int cols = 5;
+
+    vector<vector<int> > arr(rows, vector<int>(cols, 11));
+
+    for (size_t i = 0; i < arr.size(); i++) {
         for (int j=0; j<arr[i].size(); j++) {
             cout << arr[i][j] << " ";
         }
         cout << "\n";
     }
+
+    rows = 5;
+    cols = 5;
+    
+    vector<vector<int>> arr2(rows, vector<int>(cols, -8));
+
+    for (size_t i = 0; i < arr2.size(); i++) {
+        for (int j=0; j<arr2[i].size(); j++) {
+            cout << arr2[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
 }
