a16c755d02a0db0dfa0d9dab1de04ffbf0e17fa3
ApurbaKumarMajumder <9851apurba@gmail.com>
2023-05-11 11:44:04 +0530

    find_the_duplicate_number

---
+++ 2. Arrays/Practice/find_the_duplicate_number.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        // solution 1
+        // sort(nums.begin(), nums.end());
+
+        // for (int i=0; i<nums.size()-1; i++) {
+        //     if (nums[i] == nums[i+1]) {
+        //         return nums[i];
+        //     }
+        // }
+        // return -1;
+
+        // visited solutions
+        int ans = -1;
+        for (int i=0; i<nums.size(); i++) {
+            int index = abs(nums[i]);
+
+            // already visited?
+            if (nums[index] < 0) {
+                ans = index;
+                break;
+            }
+
+            // visited mark
+            nums[index] *= -1;
+        }
+        return ans;
+    }
+};
