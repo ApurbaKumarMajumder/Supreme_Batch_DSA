0e4709d79c0e6794a9eace19f17a3b9b9305ccca
Apurba Kumar Majumder <9851apurba@gmail.com>
2023-07-03 12:46:37 +0530

    precision function

--- Searching and Sorting/Practice Questions/precision.cpp
+++ Searching and Sorting/Practice Questions/precision.cpp
@@ -1,6 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int findSqrt(int n) {
+    int target = n;
+    int s = 0;
+    int e = n;
+    int mid = s + (e - s) / 2;
+    int ans = -1;
+
+    while (s <= e)
+    {
+        if (mid * mid == target)
+        {
+            return mid;
+        }
+        if (mid * mid > target)
+        {
+            // left search
+            e = mid - 1;
+        }
+        else {
+            // ans store
+            ans = mid;
+            // right search
+            s = mid + 1;
+        }
+        mid = s + (e - s) / 2;
+    }
+    return ans;
+}
+
 int binarySearch (int arr[], int size, int target) {
     int start = 0;
     int end = size - 1;
@@ -40,8 +69,21 @@ int main() {
 
     int ans = findSqrt(n);
     cout << "Ans is " << ans << endl;
+
     int precision;
     cout << "Enter the number of floating digits in precision " << endl;
+    cin >> precision;
+
+    double step = 0.1;
+    double finalAns = ans;
+
+    for (double i = 0; i < precision; i++) {
+        for (double j = finalAns; j*j <= n; j = j + step) {
+            finalAns = j;
+        }
+        step = step / 10;
+    }
+    cout << "Final ans is " << finalAns << endl;
 
     return 0;
 }
