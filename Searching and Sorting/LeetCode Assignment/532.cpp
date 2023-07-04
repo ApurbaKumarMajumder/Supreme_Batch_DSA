3f186cf10120dcf10f26443a7fef316e256b87fc
Apurba Kumar Majumder <9851apurba@gmail.com>
2023-07-05 04:38:10 +0530

    532. K-diff Pairs in an Array question done

---
+++ Searching and Sorting/LeetCode Assignment/532.cpp
@@ -0,0 +1,70 @@
+// two pointer solution approach
+
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        set<pair<int, int>> ans;
+        
+        int i = 0, j = 1;
+
+        while (j < nums.size())
+        {
+            int diff = nums[j] - nums[i];
+            if (diff == k)
+            {
+                ans.insert({nums[i++], nums[j++]});
+            }
+            else if (diff > k)
+            {
+                i++;
+            }
+            else {
+                j++;
+            }
+            if (i == j) j++;
+        }
+        return ans.size();
+    }
+};
+
+// binary search and one pointer approach
+
+class Solution {
+public:
+
+    int binarySearch(vector<int>&nums, int start, int x) {
+        int end = nums.size() - 1;
+        while (start <= end)
+        {
+            int mid = (start + end)/2;
+            if (nums[mid] == x)
+            {
+                return mid;
+            }
+            else if (x > nums[mid])
+            {
+                start = mid + 1;
+            }
+            else
+            {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    int findPairs(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        set<pair<int, int>> ans;
+        
+        for (size_t i = 0; i < nums.size(); i++)
+        {
+            if (binarySearch(nums, i+1, nums[i] + k) != -1)
+            {
+                ans.insert({nums[i], nums[i] + k});
+            }
+        }
+        return ans.size();
+    }
+};
