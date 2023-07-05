eae538a4126da4bc1b32ffc08491d535023f54c0
Apurba Kumar Majumder <9851apurba@gmail.com>
2023-07-05 14:02:17 +0530

    start with char array

--- 4. Char Arrays & Strings/intro.cpp
+++ 4. Char Arrays & Strings/intro.cpp
@@ -1,3 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int main() {
+    char name[100];
+    cout << "Enter your name " << endl;
+
+    cin >> name;
+
+    cout << "Aapka Naam " << name << " hai " << endl;
+    return 0;
+}
