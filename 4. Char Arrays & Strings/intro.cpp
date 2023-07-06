f2a98365f57f5d97678c31587c2005f7203627a0
Apurba Kumar Majumder <9851apurba@gmail.com>
2023-07-06 14:36:03 +0530

    start with char array

--- 4. Char Arrays & Strings/intro.cpp
+++ 4. Char Arrays & Strings/intro.cpp
@@ -2,11 +2,38 @@
 using namespace std;
 
 int main() {
-    char name[100];
-    cout << "Enter your name " << endl;
+    // char name[100];
+    // cout << "Enter your name " << endl;
 
-    cin >> name;
+    // cin >> name;
 
-    cout << "Aapka Naam " << name << " hai " << endl;
+    // cout << "Aapka Naam " << name << " hai " << endl;
+
+    // char ch[100];
+
+    // ch[0] = 'a';
+    // ch[1] = 'b';
+    // cin >> ch[2];
+
+    // cout << ch[0] << ch[1] << ch[2] << endl;
+
+    // char name[100];
+    // cin >> name;
+
+    // for (size_t i = 0; i < 7; i++)
+    // {
+    //     cout << "index: " << i << "value: " << name[i] << endl;
+    // }
+    
+    // int value = (int)name[6];
+    // cout << "value is: " << value << endl;
+
+    char arr[100];
+    // cin >> arr;
+    // getline(cin, arr);
+    cin.getline(arr, 50);
+
+    cout << arr;
+
     return 0;
 }
