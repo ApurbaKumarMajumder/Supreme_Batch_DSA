5f7f5ed0678f498eaba7b920a77966b03a151781
Apurba Kumar Majumder <9851apurba@gmail.com>
2023-07-06 21:00:54 +0530

    Reverse a String

--- 4. Char Arrays & Strings/intro.cpp
+++ 4. Char Arrays & Strings/intro.cpp
@@ -1,6 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int getLength(char name[]){
+    int length = 0;
+    int i = 0;
+
+    while (name[i] != '\0')
+    {
+        length++;
+        i++;
+    }
+    return length;
+}
+
+void reverseCharArray(char name[]) {
+    int i = 0;
+    int n = getLength(name);
+    int j = n - 1;
+
+    while (i <= j)
+    {
+        swap(name[i], name[j]);
+        i++;
+        j--;
+    }
+}
+
 int main() {
     // char name[100];
     // cout << "Enter your name " << endl;
@@ -28,12 +53,26 @@ int main() {
     // int value = (int)name[6];
     // cout << "value is: " << value << endl;
 
-    char arr[100];
-    // cin >> arr;
-    // getline(cin, arr);
-    cin.getline(arr, 50);
+    // char arr[100];
+    // // cin >> arr;
+    // // getline(cin, arr);
+    // cin.getline(arr, 50);
 
-    cout << arr;
+    // cout << arr;
+
+    // char name[100];
+    // cin >> name;
+
+    // cout << "length is: " << getLength(name) << endl;
+    // cout << "length is: " << strlen(name) << endl;
+
+    // 2. Reverse a String
+    char name[100];
+    cin >> name;
+    
+    cout << "Initially: " << name << endl;
+    reverseCharArray(name);
+    cout << "After reversal process: " << name << endl;
 
     return 0;
 }
