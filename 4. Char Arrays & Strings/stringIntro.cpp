9dd3b4b96ca4e4b36472f8a86e5bb08b60edf381
Apurba Kumar Majumder <9851apurba@gmail.com>
2023-07-08 11:12:07 +0530

    replace function done

--- 4. Char Arrays & Strings/stringIntro.cpp
+++ 4. Char Arrays & Strings/stringIntro.cpp
@@ -23,30 +23,52 @@ bool compareString(string a, string b) {
 
 int main() {
     // string create
-    string str;
+    // string str;
     // input
-    cin >> str;
+    // cin >> str;
     // getline(cin, str);
     // cout << str;
 
-    cout << "length: " << str.length() << endl;
-    cout << "isEmpty: " << str.empty() << endl;
-    str.push_back('A');
+    // cout << "length: " << str.length() << endl;
+    // cout << "isEmpty: " << str.empty() << endl;
+    // str.push_back('A');
+    // cout << str << endl;
+    // str.pop_back();
+    // cout << str;
+    // cout << str.substr(0, 6) << endl;
+
+    // string a = "Love";
+    // string b = "Lover";
+
+    // if (a.compare(b) == 0)
+    // {
+    //     cout << "a and b are exactly same strings " << endl;
+    // }
+    // else {
+    //     cout << "a and b are not same " << endl;
+    // }
+
+    // string x = "bbbb";
+    // string y = "bbbba";
+
+    // cout << x.compare(y) << endl;
+
+    // string sentence = "Hello Jee kaise ho saare";
+    // string target = "kaise";
+
+    // cout << sentence.find(target) << endl;
+    // if (sentence.find(target) == std::string::npos) 
+    // {
+    //     cout << "Not Found" << endl;
+    // }
+
+    string str = "This is my First Messages";
+    string word = "Babbar";
+
+    str.replace(11, 5, "Second");
     cout << str << endl;
-    str.pop_back();
-    cout << str;
-    cout << str.substr(0, 6) << endl;
 
-    string a = "Love";
-    string b = "Lover";
-
-    if (a.compare(b) == 0)
-    {
-        cout << "a and b are exactly same strings " << endl;
-    }
-    else {
-        cout << "a and b are not same " << endl;
-    }
+    // string str = "ABCDEFGHIJKSTMNLOPROST";
     
     return 0;
 }
