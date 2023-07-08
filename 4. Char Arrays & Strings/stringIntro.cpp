f28f12be3c25049aef2451865b5dac6cb3569364
Apurba Kumar Majumder <9851apurba@gmail.com>
2023-07-08 10:28:28 +0530

    compareString function done

--- 4. Char Arrays & Strings/stringIntro.cpp
+++ 4. Char Arrays & Strings/stringIntro.cpp
@@ -1,6 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool compareString(string a, string b) {
+    if (a.length() != b.length())
+    {
+        return false;
+    }
+    else
+    {
+        int j = 0;
+        for (size_t i = 0; i < a.length(); i++)
+        {
+            if (a[i] != b[j])
+            {
+                return false;
+            }
+            j++;
+        }
+        return true;
+    }
+}
+
 int main() {
     // string create
     string str;
@@ -17,4 +37,16 @@ int main() {
     cout << str;
     cout << str.substr(0, 6) << endl;
 
+    string a = "Love";
+    string b = "Lover";
+
+    if (a.compare(b) == 0)
+    {
+        cout << "a and b are exactly same strings " << endl;
+    }
+    else {
+        cout << "a and b are not same " << endl;
+    }
+    
+    return 0;
 }
